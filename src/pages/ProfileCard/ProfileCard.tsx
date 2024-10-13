beda147fd9c42a39c6d277a029c8ba166a0854dc
Iago-Sousa-Santos <iago.santos@confia.net.br>
2024-10-13 14:02:39 -0300

    fix: ajustes

--- src/pages/ProfileCard/ProfileCard.tsx
+++ src/pages/ProfileCard/ProfileCard.tsx
@@ -5,6 +5,7 @@ import userAvatar from "../../assets/user-avatar.svg";
 import PhotoUpload from "../../components/PhotoUpload";
 import { useLogin } from "../../context/AppProvider";
 import { userApi } from "../../integrations/user";
+import { blobToBase64, base64ToBlob } from "../../utils/otherUtils";
 
 interface UserInfos {
   email: string;
@@ -21,47 +22,6 @@ const ProfileCard = () => {
     userId: 0,
   });
   const { user } = useLogin();
-
-  //função para converter a imagem em base64
-  const blobToBase64 = (blob: Blob): Promise<string | null> => {
-    return new Promise((resolve, _) => {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        if (typeof reader.result === "string") {
-          resolve(reader.result.split(",")[1]);
-        } else {
-          console.error("Unexpected result type:", reader.result);
-          resolve(null);
-          // Handle ArrayBuffer case (if needed)
-        }
-      };
-      reader.readAsDataURL(blob);
-    });
-  };
-
-  //função para converter base64 em imagem
-  const base64ToBlob = async (b64Data: string): Promise<Blob> => {
-    const contentType: string = "image/jpeg";
-    const sliceSize: number = 512;
-
-    const byteCharacters = atob(b64Data);
-    const byteArrays: BlobPart[] = []; // Change to BlobPart[]
-
-    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-      const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-      const byteNumbers: number[] = new Array(slice.length);
-      for (let i = 0; i < slice.length; i++) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-
-      const byteArray = new Uint8Array(byteNumbers);
-      byteArrays.push(byteArray);
-    }
-
-    const blob = new Blob(byteArrays, { type: contentType });
-    return blob;
-  };
 
   const handleUpload = async (blob: Blob) => {
     const photo = await blobToBase64(blob);
